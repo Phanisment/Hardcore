2a7017c1d056181f397badf4f9015f972af0721b
Agil <166532433+Phanisment@users.noreply.github.com>
2024-09-04 21:03:01 +0700

    update src/main/java/io/phanisment/hardcore/mixin/ExplosionBlocker.java

--- src/main/java/io/phanisment/hardcore/mixin/ExplosionBlocker.java
+++ src/main/java/io/phanisment/hardcore/mixin/ExplosionBlocker.java
@@ -7,6 +7,7 @@ import net.minecraft.entity.damage.DamageSource;
 import net.minecraft.entity.player.PlayerEntity;
 import net.minecraft.util.math.BlockPos;
 import net.minecraft.world.World;
+
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.Inject;
