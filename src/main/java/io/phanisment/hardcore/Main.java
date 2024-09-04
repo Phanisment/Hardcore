457df5a80e707bd8020f9430790def870f0a6311
Agil <166532433+Phanisment@users.noreply.github.com>
2024-09-04 12:08:14 +0700

    update src/main/java/io/phanisment/hardcore/Main.java

--- src/main/java/io/phanisment/hardcore/Main.java
+++ src/main/java/io/phanisment/hardcore/Main.java
@@ -7,9 +7,18 @@ import org.slf4j.LoggerFactory;
 
 public class Main implements ModInitializer {
 	public static final Logger LOGGER = LoggerFactory.getLogger("hardcore");
-
+	
 	@Override
 	public void onInitialize() {
 		LOGGER.info("Hardcore?? NOPE")
+		
+		ServerTickEvents.END_SERVER_TICK.register(server -> {
+			long currentTime = System.currentTimeMillis();
+			bannedPlayers.forEach((uuid, banEndTime) -> {
+				if (currentTime >= banEndTime) {
+					bannedPlayers.remove(uuid);
+				}
+			});
+		});
 	}
 }
