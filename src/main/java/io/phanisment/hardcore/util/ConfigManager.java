f6de70f503e304eda8084fb79b734fa19b59001b
Agil <166532433+Phanisment@users.noreply.github.com>
2024-09-04 20:18:45 +0700

    update src/main/java/io/phanisment/hardcore/util/ConfigManager.java

--- src/main/java/io/phanisment/hardcore/util/ConfigManager.java
+++ src/main/java/io/phanisment/hardcore/util/ConfigManager.java
@@ -1,5 +1,34 @@
 package io.phanisment.hardcore.api;
 
-public class ConfigManager {
+import com.google.gson.Gson;
+import com.google.gson.reflect.TypeToken;
+import net.minecraft.server.network.ServerPlayerEntity;
 
+import java.io.*;
+import java.lang.reflect.Type;
+import java.util.HashMap;
+import java.util.UUID;
+
+public class ConfigManager {
+	private static final File CONFIG_FILE = new File("config/tempban.json");
+	private static final Gson GSON = new Gson();
+	public static HashMap<UUID, Long> loadBannedPlayers() {
+		if (!CONFIG_FILE.exists()) {
+			return new HashMap<>();
+		}
+		try (Reader reader = new FileReader(CONFIG_FILE)) {
+			Type type = new TypeToken<HashMap<UUID, Long>>(){}.getType();
+			return GSON.fromJson(reader, type);
+		} catch (IOException e) {
+			e.printStackTrace();
+			return new HashMap<>();
+		}
+	}
+	public static void saveBannedPlayers(HashMap<UUID, Long> bannedPlayers) {
+		try (Writer writer = new FileWriter(CONFIG_FILE)) {
+			GSON.toJson(bannedPlayers, writer);
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
+	}
 }
