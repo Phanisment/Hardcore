cc0bb8c9d7757faf85f1d859ce1d4de613c389db
Agil <166532433+Phanisment@users.noreply.github.com>
2024-09-04 12:30:56 +0700

    update src/main/java/io/phanisment/hardcore/Main.java

--- src/main/java/io/phanisment/hardcore/Main.java
+++ src/main/java/io/phanisment/hardcore/Main.java
@@ -7,12 +7,16 @@ import net.minecraft.server.network.ServerPlayerEntity;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
+import java.util.UUID;
+import java.util.concurrent.ConcurrentHashMap;
+
 public class Main implements ModInitializer {
 	public static final Logger LOGGER = LoggerFactory.getLogger("hardcore");
-	
+	public static final ConcurrentHashMap<UUID, Long> bannedPlayers = new ConcurrentHashMap<>();
+
 	@Override
 	public void onInitialize() {
-		LOGGER.info("Hardcore?? NOPE");
+		LOGGER.info("Hardcore?? Nope");
 		
 		ServerTickEvents.END_SERVER_TICK.register(server -> {
 			long currentTime = System.currentTimeMillis();
