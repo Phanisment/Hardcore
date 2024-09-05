15b88959281f2634d7da58aa271513f6d0679fa2
Agil <166532433+Phanisment@users.noreply.github.com>
2024-09-05 07:30:07 +0700

    update src/main/java/io/phanisment/hardcore/Main.java

--- src/main/java/io/phanisment/hardcore/Main.java
+++ src/main/java/io/phanisment/hardcore/Main.java
@@ -3,13 +3,9 @@ package io.phanisment.hardcore;
 import net.fabricmc.api.ModInitializer;
 import net.fabricmc.fabric.api.event.lifecycle.v1.ServerLifecycleEvents;
 import net.fabricmc.fabric.api.event.lifecycle.v1.ServerTickEvents;
-import net.fabricmc.fabric.api.networking.v1.ServerLoginConnectionEvents;
 import net.fabricmc.fabric.api.entity.event.v1.ServerPlayerEvents;
-import net.minecraft.server.network.ServerLoginNetworkHandler;
 import net.minecraft.server.network.ServerPlayerEntity;
 import net.minecraft.text.Text;
-import net.minecraft.network.ClientConnection;
-import net.minecraft.server.MinecraftServer;
 
 import java.util.UUID;
 
@@ -28,21 +24,18 @@ public class Main implements ModInitializer {
 		
 		ServerTickEvents.END_SERVER_TICK.register(server -> TempbanManager.checkUnban());
 		
-		ServerLifecycleEvents.SERVER_STARTING.register(server -> {
-			server.getNetworkIo().setConnectionHandler((ClientConnection connection) -> {
-				connection.setPacketListener(new ServerLoginNetworkHandler(server, connection));
-				ServerLoginNetworkHandler loginHandler = new ServerLoginNetworkHandler(server, connection);
-				String playerName = loginHandler.getGameProfile().getName();
-				if (TempbanManager.isBanned(playerName)) {
-					connection.disconnect(Text.of("You are temporarily banned!"));
-				}
-			});
-		});
-		
 		ServerPlayerEvents.AFTER_RESPAWN.register((oldPlayer, newPlayer, alive) -> {
 			if (oldPlayer instanceof ServerPlayerEntity) {
 				TempbanManager.tempBanPlayer((ServerPlayerEntity) oldPlayer, 20);
 			}
+		});
+		
+		ServerTickEvents.END_SERVER_TICK.register(server -> {
+			server.getPlayerManager().getPlayerList().forEach(player -> {
+				if (player instanceof ServerPlayerEntity) {
+					TempBanManager.tempBanPlayer((ServerPlayerEntity) player, 20);  // Tempban selama 20 menit
+				}
+			});
 		});
 		
 		ServerLifecycleEvents.SERVER_STOPPING.register(server -> {
