d57349add3b743a08893fd1fd017f244fe58200f
Agil <166532433+Phanisment@users.noreply.github.com>
2024-09-05 21:51:58 +0700

    update src/main/java/io/phanisment/hardcore/mixin/ServerPlayerEntity.java

--- src/main/java/io/phanisment/hardcore/mixin/ServerPlayerEntity.java
+++ src/main/java/io/phanisment/hardcore/mixin/ServerPlayerEntity.java
@@ -9,11 +9,14 @@ import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.Inject;
 import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
 
+import io.phanisment.hardcore.util.Tempban;
+
 @Mixin(ServerPlayerEntity.class)
 public abstract class ServerPlayerEntity {
 	@Inject(at = @At(value = "HEAD"), method = "onDeath", require = 1)
+	@SuppressWarnings("ConstantConditions")
 	private void onPlayerDeath(DamageSource source, CallbackInfo info) {
 		ServerPlayerEntity player = (ServerPlayerEntity) (Object) this;
-		
+		Tempban.ban(player);
 	}
 }
