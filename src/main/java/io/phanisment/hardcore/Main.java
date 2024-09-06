8e24631f496d4d968e9a64632b7d05bae5c98b0e
Agil <166532433+Phanisment@users.noreply.github.com>
2024-09-06 07:05:30 +0700

    update src/main/java/io/phanisment/hardcore/Main.java

--- src/main/java/io/phanisment/hardcore/Main.java
+++ src/main/java/io/phanisment/hardcore/Main.java
@@ -1,11 +1,50 @@
 package io.phanisment.hardcore;
 
 import net.fabricmc.api.ModInitializer;
+import net.fabricmc.fabric.api.event.lifecycle.v1.ServerTickEvents;
+import net.fabricmc.fabric.api.entity.event.v1.ServerPlayerEvents;
+import net.minecraft.server.network.ServerPlayerEntity;
+import net.minecraft.text.Text;
 
 public class Main implements ModInitializer {
+	public final HasMap<String, Long> ban = new HasMap<>()
 	
 	@Override
 	public void onInitialize() {
+		// Respwn Event
+		ServerPlayerEvents.AFTER_RESPAWN.register((player, newplayer, alive) -> {
+			if (player instanceof ServerPlayerEntity) {
+				this.ban((ServerPlayerEntity) player, 20);
+			}
+		});
 		
+		// Tick Event
+		ServerTickEvents.END_SERVER_TICK.register(server -> {
+			server.getPlayerManager().getPlayerList().forEach(player -> {
+				if (player instanceof ServerPlayerEntity) {
+					String playerName = player.getGameProfile().getName();
+					if (ban.containsKey(playerName)) {
+						long end = bannedPlayers.get(playerName);
+						if (System.currentTimeMillis() < end) {
+							player.disconnect(Text.literal("You have been temporarily banned for 20 minutes!"))
+						} else {
+							this.unban((ServerPlayerEntity) player);
+						}
+					}
+				}
+			});
+		});
+	}
+	
+	// Code Shortcut
+	public void ban(ServerPlayerEntity player, int duration) {
+		String playerName = player.getGameProfile().getName();
+		long duration = duration * 60 * 1000;
+		ban.put(playerName, System.currentTimeMillis() + duration);
+	}
+	
+	public void unban(ServerPlayerEntity player) {
+		String playerName = player.getGameProfile().getName();
+		ban.remove(playerName);
 	}
 }
